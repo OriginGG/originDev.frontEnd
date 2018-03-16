2e5ba2cc9f2bd409efe7a635bb05bd50d6bcebac
Stefan Walker <stefan.walker@gmail.com>
2018-03-16 22:55:21 +0000

    Deploy script

---
+++ src/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+rsync -avz -e "ssh -i stefan.pem" ./api ubuntu@ec2-52-86-203-150.compute-1.amazonaws.com:~/applications
+# ssh -i RLM-Private-NEW.pem ubuntu@54.191.182.21 <<'ENDSSH'
+# cd applications/web/rd1_ink
+# ./update_web_ink.sh
+# ENDSSH
