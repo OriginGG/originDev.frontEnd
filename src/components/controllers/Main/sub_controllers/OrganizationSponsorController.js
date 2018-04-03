1586122d9713e4a850fecae127a8b99c19260c5f
Stefan Walker <stefan.walker@gmail.com>
2018-04-03 12:19:02 +0100

    new carousel component

--- src/components/controllers/Main/sub_controllers/OrganizationSponsorController.js
+++ src/components/controllers/Main/sub_controllers/OrganizationSponsorController.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import injectSheet from 'react-jss';
 import { inject } from 'mobx-react';
 import PropTypes from 'prop-types';
+import AliceCarousel from 'react-alice-carousel';
 import { GlobalStyles } from 'Theme/Theme';
-import OrganizationSponsorComponentRender from '../../../render_components/OrganizationSponserComponentRender';
+// import OrganizationSponsorComponentRender from '../../../render_components/OrganizationSponserComponentRender';
 import { getSponsorsQuery } from '../../../../queries/sponsors';
 import sponsorImage1 from '../../../../assets/images/sponsor-logo1.png';
 import sponsorImage2 from '../../../../assets/images/sponsor-logo2.png';
 // import { getOrganisationQuery } from './queries/organisation'
 class OrganizationSponsorController extends Component {
-    state = { visible: false };
+    state = { currentIndex: 0, visible: false };
     componentWillMount = async () => {
+        this.timer = setInterval(() => {
+            this.moveScroll();
+        }, 3000);
         const subDomain = this.props.uiStore.current_subdomain;
         const sponsor_data = await this.props.appManager.executeQueryAuth('query', getSponsorsQuery, { subDomain });
         if (sponsor_data.resultData.edges.length > 0) {
@@ -33,21 +37,107 @@ class OrganizationSponsorController extends Component {
         }
         this.setState({ visible: true });
     }
+    mouse_over = false;
+    componentWillUnMount = () => {
+        clearInterval(this.timer);
+    }
+
+    moveScroll = () => {
+        if (this.mouse_over === false) {
+            let x = this.state.currentIndex;
+            if (x > 2) {
+                x = 0;
+            }
+            this.setState({ currentIndex: x + 1 });
+        }
+    }
+    mouseEnter = () => {
+        this.mouse_over = true;
+    }
+    mouseLeave = () => {
+        this.mouse_over = false;
+    }
     render() {
         if (this.state.visible === false) {
             return null;
         }
-        return <OrganizationSponsorComponentRender
-            sponsor_image1={this.sponsor_image1}
-            sponsor_image2={this.sponsor_image2}
-            sponsor_image3={this.sponsor_image3}
-            sponsor_image4={this.sponsor_image4}
-        />;
+        return (
+            <div onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave} style={{ height: 39 }}>
+                <AliceCarousel
+                    duration={1000}
+                    infinite={true}
+                    buttonsDisabled={true}
+                    dotsDisabled={true}
+                    fadeOutAnimation={false}
+                    mouseDragEnabled={false}
+                    playButtonEnabled={false}
+                    slideToIndex={this.state.currentIndex}
+                >
+                    <div>
+                        <div className="container col-sm-4" style={{ width: 274, float: 'left' }}>
+                            <img src={this.sponsor_image1} alt="Change Logo" className={this.props.classes.header_logo} style={{ height: 35 }} />
+                        </div>
+                        <div className="container col-sm-4" style={{ width: 274, float: 'left' }}>
+                            <img src={this.sponsor_image2} alt="Change Logo" className={this.props.classes.header_logo} style={{ height: 35 }} />
+                        </div>
+                        <div className="container col-sm-4" style={{ width: 274, float: 'left' }}>
+                            <img src={this.sponsor_image3} alt="Change Logo" className={this.props.classes.header_logo} style={{ height: 35 }} />
+                        </div>
+                        <div className="container col-sm-4" style={{ width: 274, float: 'left' }}>
+                            <img src={this.sponsor_image4} alt="Change Logo" className={this.props.classes.header_logo} style={{ height: 35 }} />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="container col-sm-4" style={{ width: 274, float: 'left' }}>
+                            <img src={this.sponsor_image1} alt="Change Logo" className={this.props.classes.header_logo} style={{ height: 35 }} />
+                        </div>
+                        <div className="container col-sm-4" style={{ width: 274, float: 'left' }}>
+                            <img src={this.sponsor_image2} alt="Change Logo" className={this.props.classes.header_logo} style={{ height: 35 }} />
+                        </div>
+                        <div className="container col-sm-4" style={{ width: 274, float: 'left' }}>
+                            <img src={this.sponsor_image3} alt="Change Logo" className={this.props.classes.header_logo} style={{ height: 35 }} />
+                        </div>
+                        <div className="container col-sm-4" style={{ width: 274, float: 'left' }}>
+                            <img src={this.sponsor_image4} alt="Change Logo" className={this.props.classes.header_logo} style={{ height: 35 }} />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="container col-sm-4" style={{ width: 274, float: 'left' }}>
+                            <img src={this.sponsor_image1} alt="Change Logo" className={this.props.classes.header_logo} style={{ height: 35 }} />
+                        </div>
+                        <div className="container col-sm-4" style={{ width: 274, float: 'left' }}>
+                            <img src={this.sponsor_image2} alt="Change Logo" className={this.props.classes.header_logo} style={{ height: 35 }} />
+                        </div>
+                        <div className="container col-sm-4" style={{ width: 274, float: 'left' }}>
+                            <img src={this.sponsor_image3} alt="Change Logo" className={this.props.classes.header_logo} style={{ height: 35 }} />
+                        </div>
+                        <div className="container col-sm-4" style={{ width: 274, float: 'left' }}>
+                            <img src={this.sponsor_image4} alt="Change Logo" className={this.props.classes.header_logo} style={{ height: 35 }} />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="container col-sm-4" style={{ width: 274, float: 'left' }}>
+                            <img src={this.sponsor_image1} alt="Change Logo" className={this.props.classes.header_logo} style={{ height: 35 }} />
+                        </div>
+                        <div className="container col-sm-4" style={{ width: 274, float: 'left' }}>
+                            <img src={this.sponsor_image2} alt="Change Logo" className={this.props.classes.header_logo} style={{ height: 35 }} />
+                        </div>
+                        <div className="container col-sm-4" style={{ width: 274, float: 'left' }}>
+                            <img src={this.sponsor_image3} alt="Change Logo" className={this.props.classes.header_logo} style={{ height: 35 }} />
+                        </div>
+                        <div className="container col-sm-4" style={{ width: 274, float: 'left' }}>
+                            <img src={this.sponsor_image4} alt="Change Logo" className={this.props.classes.header_logo} style={{ height: 35 }} />
+                        </div>
+                    </div>
+                </AliceCarousel>
+            </div>
+        );
     }
 }
 OrganizationSponsorController.propTypes = {
     uiStore: PropTypes.object.isRequired,
-    appManager: PropTypes.object.isRequired
+    appManager: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired
 };
 // LoginController.propTypes = {
 //     // uiStore: PropTypes.object.isRequired,
--- src/components/render_components/OrganizationSponserComponentRender.js
+++ src/components/render_components/OrganizationSponserComponentRender.js
@@ -7,42 +7,7 @@ import { GlobalStyles } from 'Theme/Theme';
 class OrganizationSponserComponentRender extends Component {
     render( ) {
         return (
-            <div>
-                <marquee behavior="scroll" direction="left" scrollamount="20">
-                    <div style={{
-                        width: '274px',
-                        float: 'left'
-                    }} className="container col-sm-4">
-                        <img alt="Change Logo" style={{
-                            height: '35px'
-                        }} className={this.props.classes.header_logo} src={this.props.sponsor_image1}/>
-                    </div>
-                    <div style={{
-                        width: '274px',
-                        float: 'left'
-                    }} className="container col-sm-4">
-                        <img alt="Change Logo" style={{
-                            height: '35px'
-                        }} className={this.props.classes.header_logo} src={this.props.sponsor_image2}/>
-                    </div>
-                    <div style={{
-                        width: '274px',
-                        float: 'left'
-                    }} className="container col-sm-4">
-                        <img alt="Change Logo" style={{
-                            height: '35px'
-                        }} className={this.props.classes.header_logo} src={this.props.sponsor_image3}/>
-                    </div>
-                    <div style={{
-                        width: '274px',
-                        float: 'left'
-                    }} className="container col-sm-4">
-                        <img alt="Change Logo" style={{
-                            height: '35px'
-                        }} className={this.props.classes.header_logo} src={this.props.sponsor_image4}/>
-                    </div>
-                </marquee>
-            </div>
+            <div >{this.props.sponsor_content}</div>
         )
     }
 }
