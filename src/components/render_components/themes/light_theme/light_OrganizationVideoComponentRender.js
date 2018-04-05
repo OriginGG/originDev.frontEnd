540004c5251f265ecee64669fa41a6feead3dfb1
Stefan Walker <stefan.walker@gmail.com>
2018-04-05 11:38:02 +0100

    Video fix

--- src/components/render_components/themes/light_theme/light_OrganizationVideoComponentRender.js
+++ src/components/render_components/themes/light_theme/light_OrganizationVideoComponentRender.js
@@ -8,34 +8,36 @@ class LightOrganizationVideoComponentRender extends Component {
     render( ) {
         return (
             <div>
-                <div className={this.props.classes.video_inner_main}>
-                    <h2 style={{
-                        color: 'rgb(0, 0, 0)'
-                    }}>
-                        FEATURED VIDEO
-                        <span>
-                            <a href="#" style={{
-                                color: 'rgb(0, 0, 0)',
-                                float: 'right',
-                                fontSize: '.8em'
-                            }}>More Video</a>
-                        </span>
-                    </h2>
-                    <iframe title="ifr" width="100%" height="500px" frameBorder="0" allowFullScreen src={this.props.video1_url}/>
-                    <div className={this.props.classes.small_video_container}>
-                        <div className={this.props.classes.small_video}>
-                            <div>
-                                <iframe title="ifr" width="100%" height="200px" frameBorder="0" allowFullScreen src={this.props.video2_url}/>
+                <div className={this.props.classes.video_main_container}>
+                    <div className={this.props.classes.video_inner_main}>
+                        <h2 style={{
+                            color: 'rgb(0, 0, 0)'
+                        }}>
+                            FEATURED VIDEO
+                            <span>
+                                <a href="#" style={{
+                                    color: 'rgb(0, 0, 0)',
+                                    float: 'right',
+                                    fontSize: '.8em'
+                                }}>More Video</a>
+                            </span>
+                        </h2>
+                        <iframe title="ifr" width="100%" height="500px" frameBorder="0" allowFullScreen src={this.props.video1_url}/>
+                        <div className={this.props.classes.small_video_container}>
+                            <div className={this.props.classes.small_video}>
+                                <div>
+                                    <iframe title="ifr" width="100%" height="200px" frameBorder="0" allowFullScreen src={this.props.video2_url}/>
+                                </div>
                             </div>
-                        </div>
-                        <div className={this.props.classes.small_video_middle}>
-                            <div>
-                                <iframe title="ifr" width="100%" height="200px" frameBorder="0" allowFullScreen src={this.props.video3_url}/>
+                            <div className={this.props.classes.small_video_middle}>
+                                <div>
+                                    <iframe title="ifr" width="100%" height="200px" frameBorder="0" allowFullScreen src={this.props.video3_url}/>
+                                </div>
                             </div>
-                        </div>
-                        <div className={this.props.classes.small_video}>
-                            <div>
-                                <iframe title="ifr" width="100%" height="200px" frameBorder="0" allowFullScreen src={this.props.video4_url}/>
+                            <div className={this.props.classes.small_video}>
+                                <div>
+                                    <iframe title="ifr" width="100%" height="200px" frameBorder="0" allowFullScreen src={this.props.video4_url}/>
+                                </div>
                             </div>
                         </div>
                     </div>
