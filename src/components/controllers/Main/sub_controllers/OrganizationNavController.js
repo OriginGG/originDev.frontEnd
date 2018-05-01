0e52ab22e7ae4e011e796ff65fd6e954384eebcc
Stefan Walker <stefan.walker@gmail.com>
2018-05-01 19:20:14 +0100

    Fix for light social icons

--- src/components/controllers/Main/sub_controllers/OrganizationNavController.js
+++ src/components/controllers/Main/sub_controllers/OrganizationNavController.js
@@ -26,41 +26,52 @@ class OrganizationNavController extends Component {
             return null;
         }
         const { OrganizationNavComponentRender } = this.state;
-        const theme = this.props.uiStore.current_organisation.themeId;
-        const social_item_class = `${theme}_social_menu_item`;
         const social_links = [];
         if (this.props.uiStore.current_organisation.twitterFeedUsername) {
-            social_links.push(<div key="social_item1" className={this.props.classes[social_item_class]}>
-                <i
-                    role="menuItem"
-                    tabIndex={-1}
-                    onClick={() => {
-                        const p_string = `https://twitter.com/${this.props.uiStore.current_organisation.twitterFeedUsername}`;
-                        this.openPage(p_string);
-                    }}
-                    className="fa fa-twitter" />
-            </div>);
+            social_links.push(<i
+                key="social_item1"
+                role="menuItem"
+                tabIndex={-1}
+                onClick={() => {
+                    const p_string = `https://twitter.com/${this.props.uiStore.current_organisation.twitterFeedUsername}`;
+                    this.openPage(p_string);
+                }}
+                className="fa fa-twitter" />);
         }
         if (this.props.uiStore.current_organisation.fbLink) {
-            social_links.push(<div key="social_item2" className={this.props.classes[social_item_class]}>
-                <i role="menuItem" tabIndex={-1} onClick={() => { this.openPage(this.props.uiStore.current_organisation.fbLink); }} className="fa fa-facebook" />
-            </div>);
+            social_links.push(<i key="social_item2" role="menuItem" tabIndex={-1} onClick={() => { this.openPage(this.props.uiStore.current_organisation.fbLink); }} className="fa fa-facebook" />);
         }
         if (this.props.uiStore.current_organisation.instaLink) {
-            social_links.push(<div key="social_item3" className={this.props.classes[social_item_class]}>
-                <i role="menuItem" tabIndex={-1} onClick={() => { this.openPage(this.props.uiStore.current_organisation.instaLink); }} className="fa fa-instagram" />
-            </div>);
+            social_links.push(<i key="social_item3" role="menuItem" tabIndex={-1} onClick={() => { this.openPage(this.props.uiStore.current_organisation.instaLink); }} className="fa fa-instagram" />);
         }
         if (this.props.uiStore.current_organisation.twitchLink) {
-            social_links.push(<div key="social_item4" className={this.props.classes[social_item_class]}>
-                <i role="menuItem" tabIndex={-1} onClick={() => { this.openPage(this.props.uiStore.current_organisation.twitchLink); }} className="fa fa-twitch" />
-            </div>);
+            social_links.push(<i key="social_item4" role="menuItem" tabIndex={-1} onClick={() => { this.openPage(this.props.uiStore.current_organisation.twitchLink); }} className="fa fa-twitch" />);
         }
         if (this.props.uiStore.current_organisation.youtubeLink) {
-            social_links.push(<div key="social_item5" className={this.props.classes[social_item_class]}>
-                <i role="menuItem" tabIndex={-1} onClick={() => { this.openPage(this.props.uiStore.current_organisation.youtubeLink); }} className="fa fa-youtube" />
-            </div>);
+            social_links.push(<i key="social_item5" role="menuItem" tabIndex={-1} onClick={() => { this.openPage(this.props.uiStore.current_organisation.youtubeLink); }} className="fa fa-youtube" />);
         }
+        let social_link1 = <span />;
+        let social_link2 = <span />;
+        let social_link3 = <span />;
+        let social_link4 = <span />;
+        let social_link5 = <span />;
+
+        if (social_links.length > 4) {
+            social_link5 = social_links[4];          // eslint-disable-line
+        }
+        if (social_links.length > 3) {
+            social_link4 = social_links[3];           // eslint-disable-line
+        }
+        if (social_links.length > 2) {
+            social_link3 = social_links[2];           // eslint-disable-line
+        }
+        if (social_links.length > 1) {
+            social_link2 = social_links[1];           // eslint-disable-line
+        }
+        if (social_links.length > 0) {
+            social_link1 = social_links[0];           // eslint-disable-line
+        }
+
         return <OrganizationNavComponentRender
             login_style={this.props.login_style}
             home_style={this.props.home_style}
@@ -70,7 +81,11 @@ class OrganizationNavController extends Component {
             handleStoreClick={this.props.handleStoreClick}
             handleAboutClick={this.props.handleAboutClick}
             handleLoginClick={this.props.handleLoginClick}
-            social_links={social_links}
+            social_link1={social_link1}
+            social_link2={social_link2}
+            social_link3={social_link3}
+            social_link4={social_link4}
+            social_link5={social_link5}
             image_src={this.image_src} />;
     }
 }
@@ -81,7 +96,6 @@ OrganizationNavController.propTypes = {
     handleStoreClick: PropTypes.func.isRequired,
     handleLoginClick: PropTypes.func.isRequired,
     uiStore: PropTypes.object.isRequired,
-    classes: PropTypes.object.isRequired,
     about_style: PropTypes.object.isRequired,
     store_style: PropTypes.object.isRequired,
     home_style: PropTypes.object.isRequired,
