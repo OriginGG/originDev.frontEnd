f41adf2599d26bdded461bb5219183909d2ef6c7
Yukun <yukunpengba@gmail.com>
2018-05-22 20:02:51 -0500

    added react component and GA script

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import { Provider } from 'mobx-react';
 import { ApolloProvider } from 'react-apollo';
 import { Router } from 'react-router-dom';
@@ -11,6 +12,17 @@ import App from './App';
 import appManager from './utils/appManager';
 import registerServiceWorker from './registerServiceWorker';
 import { GlobalTheme } from './utils/themes/Theme';
+
+export const initGA = () => {
+    console.log('Google Analytics Starts!');
+    ReactGA.initialize('UA-119536253-1');
+  };
+export const logPageView = () => {
+    console.log({ page: window.location.pathname });
+    console.log(window.location.pathname);
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+  };
 
 require('default-passive-events');
 
