b100081cc8aa8abdad994e6c132870867b2ac9f7
System Administrator <root@kyles-MacBook-Pro.local>
2018-06-05 23:59:08 -0700

    added Staff to admin menu section OR-482

---
+++ src/components/controllers/Admin/sub_controllers/data/AllPositions.js
@@ -0,0 +1,43 @@
+import coc_image from '../../../../../assets/images/game_images/clashofclans.png';
+import cod_image from '../../../../../assets/images/game_images/cod.png';
+import dota_image from '../../../../../assets/images/game_images/dota2.png';
+import csgo_image from '../../../../../assets/images/game_images/csgo.png';
+import fortnite_image from '../../../../../assets/images/game_images/fortnite.jpg';
+import hearthstone_image from '../../../../../assets/images/game_images/hearthstone.png';
+import lol_image from '../../../../../assets/images/game_images/lol.jpg';
+import paladins_image from '../../../../../assets/images/game_images/paladins.jpg';
+import ow_image from '../../../../../assets/images/game_images/ow.png';
+import pubg_image from '../../../../../assets/images/game_images/pubg.png';
+import smite_image from '../../../../../assets/images/game_images/smite.png';
+import splatoon_image from '../../../../../assets/images/game_images/splatoon.png';
+import vainglory_image from '../../../../../assets/images/game_images/vainglory.png';
+import wow_image from '../../../../../assets/images/game_images/wow.png';
+import r6_image from '../../../../../assets/images/game_images/r6.png';
+
+export const positionOptions = [
+    {
+        position_id: 1,
+        text: 'CEO',
+        value: 'CEO'
+    },
+    {
+        position_id: 2,
+        text: 'CTO',
+        value: 'CTO'
+    },
+    {
+        position_id: 3,
+        text: 'CFO',
+        value: 'CFO'
+    },
+    {
+        position_id: 4,
+        text: 'Manager',
+        value: 'Manager'
+    },
+    {
+        position_id: 5,
+        text: 'Support',
+        value: 'Support'
+    },
+];
