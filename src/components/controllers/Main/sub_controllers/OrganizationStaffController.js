e8b6c8d8fddab2f6bd9b8b31a08dda53f0995f14
System Administrator <root@kyles-MacBook-Pro.local>
2018-06-29 11:57:54 -0700

    OR-507 staff list now clickable

--- src/components/controllers/Main/sub_controllers/OrganizationStaffController.js
+++ src/components/controllers/Main/sub_controllers/OrganizationStaffController.js
@@ -47,17 +47,17 @@ class OrganizationStaffController extends Component {
     // }
 
     handleClick = (i) => {              // eslint-disable-line
-        // const x = this.props.appManager.getDomainInfo();
-        // let p = x.hostname;
-        // if (p.indexOf(x.subDomain) > -1) {
-        //     p = p.substr(x.subDomain.length + 1, p.length);
-        //     let pt = '';
-        //     if (x.port) {
-        //         pt = `:${x.port}`;
-        //     }
-        //     const url = `${x.protocol}//${p}${pt}/individual?u=${i}`;
-        //     window.open(url, '_blank');
-        // }
+        const x = this.props.appManager.getDomainInfo();
+        let p = x.hostname;
+        if (p.indexOf(x.subDomain) > -1) {
+            p = p.substr(x.subDomain.length + 1, p.length);
+            let pt = '';
+            if (x.port) {
+                pt = `:${x.port}`;
+            }
+            const url = `${x.protocol}//${p}${pt}/individual?u=${i}`;
+            window.open(url, '_blank');
+        }
     }
 
     handle_social = (s, ind_user) => {
