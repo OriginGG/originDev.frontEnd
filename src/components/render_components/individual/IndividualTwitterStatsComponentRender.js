360baf78fc0ef5b831cc063ddcf7716ae3a56715
Yukun <yukunpengba@gmail.com>
2018-08-09 17:33:27 -0500

    fixing twitter components html

--- src/components/render_components/individual/IndividualTwitterStatsComponentRender.js
+++ src/components/render_components/individual/IndividualTwitterStatsComponentRender.js
@@ -16,24 +16,26 @@ class IndividualTwitterStatsComponentRender extends Component {
                     }}>
                         Twitter Stats
                     </h2>
-                </div>
-                <div id="twitter_stats" className={this.props.classes.individual_social_content}>
-                    <h2 className={this.props.classes.individual_social_empty}>{this.props.username}</h2>
-                    <h2 className={this.props.classes.individual_social_empty}/>
-                </div>
-                <div id="twitter_followers_count" className={this.props.classes.indiviual_stats_container}>
-                    <span className={this.props.classes.individual_stats_title}>Followers:
-                    </span>
-                    <span className={this.props.classes.individual_stats_data}>{this.props.twitter_followers_count}</span>
-                </div>
-                <div id="twitter_statues_count" className={this.props.classes.indiviual_stats_container}>
-                    <span className={this.props.classes.individual_stats_title}>Statuses:
-                    </span>
-                    <span className={this.props.classes.individual_stats_data}>{this.props.twitter_statues_count}</span>
-                </div>
-                <div id="twitter_favorites_count" className={this.props.classes.indiviual_stats_container}>
-                    <span className={this.props.classes.individual_stats_title}>Favorites:</span>
-                    <span className={this.props.classes.individual_stats_data}>{this.props.twitter_favorites_count}</span>
+                    <div id="twitter_stats" className={this.props.classes.individual_social_content}>
+                        <h2 className={this.props.classes.individual_social_empty}>{this.props.twitter_username}</h2>
+                    </div>
+                    <div id="twitter_screen_name" className={this.props.classes.individual_social_content}>
+                        <h2 className={this.props.classes.individual_social_empty}>{this.props.twitter_screen_name}</h2>
+                    </div>
+                    <div id="twitter_followers_count" className={this.props.classes.indiviual_stats_container}>
+                        <span className={this.props.classes.individual_stats_title}>Followers:
+                        </span>
+                        <span className={this.props.classes.individual_stats_data}>{this.props.twitter_followers_count}</span>
+                    </div>
+                    <div id="twitter_status_count" className={this.props.classes.indiviual_stats_container}>
+                        <span className={this.props.classes.individual_stats_title}>Statuses:
+                        </span>
+                        <span className={this.props.classes.individual_stats_data}>{this.props.twitter_status_count}</span>
+                    </div>
+                    <div id="twitter_favorites_count" className={this.props.classes.indiviual_stats_container}>
+                        <span className={this.props.classes.individual_stats_title}>Favorites:</span>
+                        <span className={this.props.classes.individual_stats_data}>{this.props.twitter_favourite_count}</span>
+                    </div>
                 </div>
             </div>
         )
