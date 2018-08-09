4527630d9be0f97d74c98a59b4d5e7119b55fe9c
System Administrator <root@kyles-MacBook-Pro.local>
2018-08-09 14:20:54 -0700

    added cursor pointer to news and sponsor

--- src/components/render_components/individual/IndividualTwitterStatsComponentRender.js
+++ src/components/render_components/individual/IndividualTwitterStatsComponentRender.js
@@ -16,7 +16,24 @@ class IndividualTwitterStatsComponentRender extends Component {
                     }}>
                         Twitter Stats
                     </h2>
-                    <div className={this.props.classes.individual_social_content}>{this.props.twitter_stats}</div>
+                </div>
+                <div id="twitter_stats" className={this.props.classes.individual_social_content}>
+                    <h2 className={this.props.classes.individual_social_empty}>{this.props.username}</h2>
+                    <h2 className={this.props.classes.individual_social_empty}/>
+                </div>
+                <div id="twitter_followers_count" className={this.props.classes.indiviual_stats_container}>
+                    <span className={this.props.classes.individual_stats_title}>Followers:
+                    </span>
+                    <span className={this.props.classes.individual_stats_data}>{this.props.twitter_followers_count}</span>
+                </div>
+                <div id="twitter_statues_count" className={this.props.classes.indiviual_stats_container}>
+                    <span className={this.props.classes.individual_stats_title}>Statuses:
+                    </span>
+                    <span className={this.props.classes.individual_stats_data}>{this.props.twitter_statues_count}</span>
+                </div>
+                <div id="twitter_favorites_count" className={this.props.classes.indiviual_stats_container}>
+                    <span className={this.props.classes.individual_stats_title}>Favorites:</span>
+                    <span className={this.props.classes.individual_stats_data}>{this.props.twitter_favorites_count}</span>
                 </div>
             </div>
         )
