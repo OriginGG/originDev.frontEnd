91477a2ef14757a1cdac8eda97bace95514bc8d9
System Administrator <root@kyles-MacBook-Pro.local>
2018-08-27 22:41:47 -0700

    added discord icon to social nav

--- src/components/render_components/themes/enigma/light/OrganizationNavComponentRender.js
+++ src/components/render_components/themes/enigma/light/OrganizationNavComponentRender.js
@@ -17,6 +17,7 @@ class ObliviotOrganizationNavComponentRender extends Component {
                                 <div className={this.props.classes.obliviot_social_menu_item}>{this.props.social_link3}</div>
                                 <div className={this.props.classes.obliviot_social_menu_item}>{this.props.social_link4}</div>
                                 <div className={this.props.classes.obliviot_social_menu_item}>{this.props.social_link5}</div>
+                                <div className={this.props.classes.obliviot_social_menu_item}>{this.props.social_link6}</div>
                             </div>
                         </div>
                     </div>
