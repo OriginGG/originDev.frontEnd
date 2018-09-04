1040e3c7cbb8698bfe36660d6196aced4808a03b
Stefan Walker <stefan.walker@gmail.com>
2018-09-04 15:28:53 +0100

    Fix sponsor creation

--- src/components/controllers/Admin/sub_controllers/AdminSponsorController.js
+++ src/components/controllers/Admin/sub_controllers/AdminSponsorController.js
@@ -9,7 +9,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 // import OrganizationAdminSponsorComponentRender from '../../../render_components/admin/OrganizationAdminSponserComponentRender';
 import OrganizationAdminSponsorComponentElementRender from '../../../render_components/admin/OrganizationAdminSponserComponentElementRender';
-import { getSponsorsQuery, updateSponsorsQuery, createSponsorsQuery } from '../../../../queries/sponsors';
+import { getSponsorsQuery, updateSponsorsQuery } from '../../../../queries/sponsors';
 // import blankImage from '../../../../assets/images/imgPlaceholder1.png';
 // import disableImage from '../../../../assets/images/element-disabled.png';
 
@@ -125,12 +125,6 @@ class SponsorBlock extends Component {
 class AdminSponsorController extends Component {
     state = {
         element_array: [],
-        input_values: {
-            sponsor_image: null,
-            http_link_value: '',
-            sponsor_desc_value: '',
-            sponsor_name_value: '',
-        }
     };
     componentDidMount = async () => {
         this.subscribed = true;
@@ -154,113 +148,6 @@ class AdminSponsorController extends Component {
             />);
         });
         this.setState({ element_array: p_array });
-    }
-
-
-    handleSubmit = async () => {
-        const sponsor_image1 = await this.uploadSponsorMedia('sponsor_image1');
-        const sponsor_image2 = await this.uploadSponsorMedia('sponsor_image2');
-        const sponsor_image3 = await this.uploadSponsorMedia('sponsor_image3');
-        const sponsor_image4 = await this.uploadSponsorMedia('sponsor_image4');
-        const sponser_href1 = this.state.input_values.http_link1_value;
-        const sponser_href2 = this.state.input_values.http_link2_value;
-        const sponser_href3 = this.state.input_values.http_link3_value;
-        const sponser_href4 = this.state.input_values.http_link4_value;
-
-        if (!this.isURL(sponser_href1) && sponser_href1) {
-            toast.error('First Sponser URL Not Valid', {
-                position: toast.POSITION.TOP_LEFT
-            });
-            return;
-        }
-        if (!this.isURL(sponser_href2) && sponser_href2) {
-            toast.error('Second Sponser URL Not Valid', {
-                position: toast.POSITION.TOP_LEFT
-            });
-            return;
-        }
-        if (!this.isURL(sponser_href3) && sponser_href3) {
-            toast.error('Third Sponser URL Not Valid', {
-                position: toast.POSITION.TOP_LEFT
-            });
-            return;
-        }
-        if (!this.isURL(sponser_href4) && sponser_href4) {
-            toast.error('Fourth Sponser URL Not Valid', {
-                position: toast.POSITION.TOP_LEFT
-            });
-            return;
-        }
-        if (this.create) {
-            await this.props.appManager.executeQuery(
-                'mutation', createSponsorsQuery,
-                {
-                    subDomain: this.props.uiStore.current_organisation.subDomain,
-                    link1: sponsor_image1,
-                    link2: sponsor_image2,
-                    link3: sponsor_image3,
-                    link4: sponsor_image4,
-                    href_link1: sponser_href1,
-                    href_link2: sponser_href2,
-                    href_link3: sponser_href3,
-                    href_link4: sponser_href4,
-                    desc1: this.state.input_values.sponsor_desc1_value,
-                    desc2: this.state.input_values.sponsor_desc2_value,
-                    desc3: this.state.input_values.sponsor_desc3_value,
-                    desc4: this.state.input_values.sponsor_desc4_value,
-                    name1: this.state.input_values.sponsor_name1_value,
-                    name2: this.state.input_values.sponsor_name2_value,
-                    name3: this.state.input_values.sponsor_name3_value,
-                    name4: this.state.input_values.sponsor_name4_value
-
-                }
-            );
-            toast.success('Sponsors Updated!', {
-                position: toast.POSITION.TOP_LEFT
-            });
-        } else {
-            await this.props.appManager.executeQuery(
-                'mutation', updateSponsorsQuery,
-                {
-                    id: this.c_id,
-                    link1: sponsor_image1,
-                    link2: sponsor_image2,
-                    link3: sponsor_image3,
-                    link4: sponsor_image4,
-                    href_link1: sponser_href1,
-                    href_link2: sponser_href2,
-                    href_link3: sponser_href3,
-                    href_link4: sponser_href4,
-                    desc1: this.state.input_values.sponsor_desc1_value,
-                    desc2: this.state.input_values.sponsor_desc2_value,
-                    desc3: this.state.input_values.sponsor_desc3_value,
-                    desc4: this.state.input_values.sponsor_desc4_value,
-                    name1: this.state.input_values.sponsor_name1_value,
-                    name2: this.state.input_values.sponsor_name2_value,
-                    name3: this.state.input_values.sponsor_name3_value,
-                    name4: this.state.input_values.sponsor_name4_value
-                }
-            );
-            toast.success('Sponsors Updated!', {
-                position: toast.POSITION.TOP_LEFT
-            });
-        }
-    }
-
-
-    uploadFile = (field, e) => {
-        console.log(field);
-        const { files } = e.target;
-        this.logo_files[field] = files[0];             // eslint-disable-line
-        const reader = new FileReader();
-        reader.readAsDataURL(this.logo_files[field]);
-
-        reader.onloadend = () => {
-            const x = reader.result;
-            const p = this.state.input_values;
-            p[field] = x;
-            this.setState(p);
-        };
     }
     showSubscribeConfirm = () => {
         return new Promise(resolve => {
--- src/queries/sponsors.js
+++ src/queries/sponsors.js
@@ -70,33 +70,19 @@ export const updateSponsorsQuery = gql`mutation updateSponsor($id: Int!, $imageU
   }
 }`;
 
-export const createSponsorsQuery = gql`mutation createsponsor($subDomain: String!, $link1: String, $link2: String, $link3: String, $link4: String,
-$href_link1: String, $href_link2: String, $href_link3: String, $href_link4: String,
-$desc1: String, $desc2: String, $desc3: String, $desc4: String, $name1: String, $name2: String, $name3: String, $name4: String
-) {
-  createSponsor(input:{
-    sponsor: {
-      organisation: $subDomain
-      sponsor1: $link1
-      sponsor2: $link2
-      sponsor3: $link3
-      sponsor4: $link4
-      hrefLink1: $href_link1
-      hrefLink2: $href_link2
-      hrefLink3: $href_link3
-      hrefLink4: $href_link4
-      sponsorDesc1: $desc1
-      sponsorDesc2: $desc2
-      sponsorDesc3: $desc3
-      sponsorDesc4: $desc4
-      sponsorName1: $name1
-      sponsorName2: $name2
-      sponsorName3: $name3
-      sponsorName4: $name4
-  }}) {
-    sponsor {
+export const createSponsorsQuery = gql`mutation createSponsorQuery($subDomain: String!, $imageUrl: String, $hrefLink: String, $description: String, $name: String) {
+  createOrgSponsor(input: {orgSponsor: {
+    imageUrl: $imageUrl
+    organisation: $subDomain
+    hrefLink: $hrefLink
+    name: $name
+    description: $description
+    }
+  }) {
+    orgSponsor {
       id
     }
   }
-}`;
+} 
+`;
 
