cde9e642f4f3afac8dfed6f8bc241ecb4e277181
Stefan Walker <stefan.walker@gmail.com>
2018-09-19 11:51:20 +0100

    Create sponsors on payment

--- src/components/controllers/Admin/sub_controllers/AdminSubscriptionController.js
+++ src/components/controllers/Admin/sub_controllers/AdminSubscriptionController.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Col, Row } from 'antd';
 import { toast } from 'react-toastify';
 import axios from 'axios';
 
@@ -16,7 +17,7 @@ import {
 import stripeImage from '../../../../assets/images/powered_by_stripe@3x.png';
 import appManager from '../../../../utils/appManager';
 import { updateUserQuery } from '../../../../queries/users';
-
+import { getSponsorsQuery, createSponsorsQuery } from '../../../../queries/sponsors';
 
 const createOptions = (fontSize: string, padding: ?string) => {
     return {
@@ -50,11 +51,69 @@ const handleReady = () => {
     console.log('[ready]');
 };
 
+// {
+// 	"id": "plan_DdGk8Fo0gmJXeB",
+// 	"object": "plan",
+// 	"active": true,
+// 	"aggregate_usage": null,
+// 	"amount": 3900,
+// 	"billing_scheme": "per_unit",
+// 	"created": 1537342490,
+// 	"currency": "usd",
+// 	"interval": "month",
+// 	"interval_count": 1,
+// 	"livemode": false,
+// 	"metadata": {},
+// 	"nickname": "Monthly Subscription",
+// 	"product": "prod_DdGjhkIK64yY3T",
+// 	"tiers": null,
+// 	"tiers_mode": null,
+// 	"transform_usage": null,
+// 	"trial_period_days": 7,
+// 	"usage_type": "licensed"
+// }
+const PlanElement = ({ plan, handleClick }) => {
+    const dollars = plan.amount / 100;
+    const f_array = [];
+    for (const k in plan.metadata) {                    // eslint-disable-line
+        f_array.push(<li className="feature"><span>{plan.metadata[k]}</span></li>);
+    }
+    const w = dollars.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+    let call = <li style={{ cursor: 'pointer' }} onClick={() => { handleClick(plan); }} className="call-to-action">Buy Now</li>;
+    if (!handleClick) {
+        call = <li style={{ cursor: 'pointer' }} onClick={() => { handleClick(plan); }} className="call-to-action-bl" />;
+    }
+    return (
+        <div className="pricing-table clearfix">
+            <ul className="highlight-high">
+                <li className="heading">{plan.nickname}</li>
+                <li className="price">{w}/{plan.interval}</li>
+                {f_array}
+                {call}
+            </ul>
+        </div>
+    );
+};
 
 class _SplitForm extends React.Component {
-    state = { visible: false };
+    state = {
+        subscribed: false, show_plan: true, actual_plan: null, visible: false
+    };
     componentDidMount = async () => {
-        this.setState({ subscribed: this.props.subscribed, visible: true });
+        const plans = await axios.get(`${process.env.REACT_APP_API_SERVER}/stripe/retrieve_plans`);
+        if (plans.data) {
+            const { data } = plans.data;
+
+            this.p_array = [];
+            data.forEach(d => {
+                this.p_array.push(<PlanElement plan={d} handleClick={this.handleBuyClick} />);
+            });
+            console.log(data);
+        }
+        this.setState({ subscribed: this.props.subscribed, show_plan: true, visible: true });
+    }
+    handleBuyClick = (plan) => {
+        this.setState({ show_plan: false, actual_plan: plan });
     }
     handleSubmit = (ev) => {
         ev.preventDefault();
@@ -73,7 +132,6 @@ class _SplitForm extends React.Component {
                             {
                                 token: payload.token.id,
                                 customer_id: this.props.user_id,
-                                plan_name: 'standard'
                             },
                             {
                                 headers: {
@@ -107,6 +165,21 @@ class _SplitForm extends React.Component {
                                     position: toast.POSITION.TOP_LEFT,
                                     autoClose: 3000
                                 });
+
+                                // add 4 more sponsors, if not already there.
+                                const sponsor_data = await appManager.executeQueryAuth('query', getSponsorsQuery, { subDomain: this.props.domain });
+                                const num_sponsors = sponsor_data.organisationAccountBySubDomain.orgSponsorsByOrganisation.nodes.length;
+                                const num_to_create = 8 - num_sponsors;
+                                for (let p = 0; p < num_to_create; p += 1) {
+                                    await appManager.executeQuery('mutation', createSponsorsQuery, {                // eslint-disable-line
+                                        subDomain: this.props.domain,
+                                        imageUrl: 'https://s3.amazonaws.com/origin-images/origin/sponsor_images/logoSameColor.png',
+                                        hrefLink: 'http://origin.gg',
+                                        name: 'Origin.GG',
+                                        description: 'Building an Esports team is difficult. Recruiting players, practicing, and getting your teams to events is a full-time job. Allow us to handle the rest. Origin.gg makes it easy for you to set up a pro style organization.'
+                                    });
+                                }
+                                console.log(num_to_create);
                                 this.props.callback();
                             }
                         }
@@ -120,56 +193,76 @@ class _SplitForm extends React.Component {
         if (this.state.visible === false) {
             return null;
         }
+        let disp = <p>We will tidy this up, but you are already subscribed!</p>;
+        if (!this.state.subscribed) {
+            if (this.state.show_plan === false) {
+                disp =
+                    <Row>
+                        <Col span={12} >
+                            <form onSubmit={this.handleSubmit}>
+                                <div style={{ height: 40, marginBottom: 16 }}><img style={{ height: 'inherit' }} alt="Powered By Stripe" src={stripeImage} /></div>
+                                <label>
+                                    Card number
+          <CardNumberElement
+                                        onBlur={handleBlur}
+                                        onChange={handleChange}
+                                        onFocus={handleFocus}
+                                        onReady={handleReady}
+                                        {...createOptions(this.props.fontSize)}
+                                    />
+                                </label>
+                                <label>
+                                    Expiration date
+          <CardExpiryElement
+                                        onBlur={handleBlur}
+                                        onChange={handleChange}
+                                        onFocus={handleFocus}
+                                        onReady={handleReady}
+                                        {...createOptions(this.props.fontSize)}
+                                    />
+                                </label>
+                                <label>
+                                    CVC
+          <CardCVCElement
+                                        onBlur={handleBlur}
+                                        onChange={handleChange}
+                                        onFocus={handleFocus}
+                                        onReady={handleReady}
+                                        {...createOptions(this.props.fontSize)}
+                                    />
+                                </label>
+                                <label>
+                                    Postal code
+          <PostalCodeElement
+                                        onBlur={handleBlur}
+                                        onChange={handleChange}
+                                        onFocus={handleFocus}
+                                        onReady={handleReady}
+                                        {...createOptions(this.props.fontSize)}
+                                    />
+                                </label>
+                                <button>Pay</button>
+                            </form>;
+                            </Col>
+                        <Col span={12}><PlanElement plan={this.state.actual_plan} /></Col>
+                    </Row>;
+            } else {
+                disp = <div>
+                    <h2 className="ui header">
+                        <i aria-hidden="true" className="dollar sign icon" />
+                        <div className="content">
+                            Available Plans
+                            <div className="sub header">Choose your plan below</div>
+                        </div>
+                    </h2>
+                    {this.p_array}
+                </div>;
+            }
+        }
+
         return (
             <div>
-                {!this.state.subscribed &&
-                    <form onSubmit={this.handleSubmit}>
-                        <label>
-                            Card number
-          <CardNumberElement
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                onFocus={handleFocus}
-                                onReady={handleReady}
-                                {...createOptions(this.props.fontSize)}
-                            />
-                        </label>
-                        <label>
-                            Expiration date
-          <CardExpiryElement
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                onFocus={handleFocus}
-                                onReady={handleReady}
-                                {...createOptions(this.props.fontSize)}
-                            />
-                        </label>
-                        <label>
-                            CVC
-          <CardCVCElement
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                onFocus={handleFocus}
-                                onReady={handleReady}
-                                {...createOptions(this.props.fontSize)}
-                            />
-                        </label>
-                        <label>
-                            Postal code
-          <PostalCodeElement
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                onFocus={handleFocus}
-                                onReady={handleReady}
-                                {...createOptions(this.props.fontSize)}
-                            />
-                        </label>
-                        <button>Pay</button>
-                    </form>
-                }
-                {this.state.subscribed &&
-                    <p>We will tidy this up, but you are already subscribed!</p>
-                }
+                {disp}
             </div>
         );
     }
@@ -184,14 +277,22 @@ class AdminSubscriptionController extends Component {
     render() {
         return (
             <div className="Checkout">
-                <div style={{ height: 40, marginBottom: 16 }}><img style={{ height: 'inherit' }} alt="Powered By Stripe" src={stripeImage} /></div>
                 <Elements>
-                    <SplitForm callback={this.props.callback} user_id={this.props.user_id} subscribed={this.props.subscribed} fontSize="14px" />
+                    <SplitForm callback={this.props.callback} domain={this.props.domain} user_id={this.props.user_id} subscribed={this.props.subscribed} fontSize="14px" />
                 </Elements>
             </div>
         );
     }
 }
+
+PlanElement.propTypes = {
+    plan: PropTypes.object.isRequired,
+    handleClick: PropTypes.func
+};
+
+PlanElement.defaultProps = {
+    handleClick: null
+};
 
 _SplitForm.propTypes = {
     user_id: PropTypes.number.isRequired,
@@ -199,12 +300,14 @@ _SplitForm.propTypes = {
     fontSize: PropTypes.string.isRequired,
     subscribed: PropTypes.bool.isRequired,
     callback: PropTypes.func.isRequired,
+    domain: PropTypes.string.isRequired
 };
 
 AdminSubscriptionController.propTypes = {
     user_id: PropTypes.number.isRequired,
     subscribed: PropTypes.bool.isRequired,
-    callback: PropTypes.func.isRequired
+    callback: PropTypes.func.isRequired,
+    domain: PropTypes.string.isRequired
 };
 
 
