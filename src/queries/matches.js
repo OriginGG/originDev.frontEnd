467457e88caf7d5d978a657fd58aa2cc3b91bed8
Stefan Walker <stefan.walker@gmail.com>
2018-09-28 16:59:17 +0100

    Recent matches upload catch problem

--- src/queries/matches.js
+++ src/queries/matches.js
@@ -1,9 +1,9 @@
 import gql from 'graphql-tag';
 
 export const createRecentMatchQuery = gql`mutation createRecentMatch($subDomain: String!, $oppositeTeamName: String, $gameName: String!,
-  $gameLogo:String!, $score: String!) {
+  $gameLogo:String!, $score: String!, $eventDescription: String) {
   createRecentmatch(input:{recentmatch:{organisation:$subDomain, oppositeTeamName: $oppositeTeamName,
-  gameName: $gameName, score: $score, gameLogo: $gameLogo}}) {
+  gameName: $gameName, score: $score, gameLogo: $gameLogo, eventDescription: $eventDescription}}) {
     recentmatch {
       id
     }
@@ -18,21 +18,21 @@ export const deleteRecentMatchQuery = gql`mutation deleteRecentMatch($id: Int!) 
 }`;
 
 export const recentMatchesQuery = gql`
-   query recentMatches($organisation: String!) {
-    resultdata: allRecentmatches(orderBy: CREATED_AT_DESC, condition: { organisation: $organisation }) {
-        edges {
-            node
-            {
-                id
-                organisation
-                oppositeTeamName
-                oppositeTeamLogo
-                gameName
-                gameLogo
-                score
-                createdAt
-            }
-        }
-    }
-}
-`;
+         query recentMatches($organisation: String!) {
+           resultdata: allRecentmatches(orderBy: CREATED_AT_DESC, condition: { organisation: $organisation }) {
+             edges {
+               node {
+                 id
+                 organisation
+                 oppositeTeamName
+                 oppositeTeamLogo
+                 gameName
+                 gameLogo
+                 score
+                 createdAt
+                 eventDescription
+               }
+             }
+           }
+         }
+       `;
