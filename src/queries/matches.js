8ad7087693478abb9887c7a96b91177b5c62f242
Stefan Walker <stefan.walker@gmail.com>
2018-10-08 17:03:51 +0100

    More

--- src/queries/matches.js
+++ src/queries/matches.js
@@ -1,15 +1,9 @@
 import gql from 'graphql-tag';
 
 export const createRecentMatchQuery = gql`mutation createRecentMatch($subDomain: String!, $oppositeTeamName: String, $gameName: String!,
-<<<<<<< HEAD
-  $gameLogo:String!, $score: String!, $eventDescription: String, $type: String) {
-  createRecentmatch(input:{recentmatch:{organisation:$subDomain, oppositeTeamName: $oppositeTeamName,
-  gameName: $gameName, score: $score, gameLogo: $gameLogo, eventDescription: $eventDescription, type: $type}}) {
-=======
   $gameLogo:String!, $score: String!, $eventDescription: String, $eventDate: String, $eventUrl: String, $eventInfo: String) {
   createRecentmatch(input:{recentmatch:{organisation:$subDomain, oppositeTeamName: $oppositeTeamName,
   gameName: $gameName, score: $score, gameLogo: $gameLogo, eventDescription: $eventDescription, eventUrl: $eventUrl, eventInfo: $eventInfo, eventDate: $eventDate}}) {
->>>>>>> daff722471f3dc4fdf7975d627d0eab51106ca26
     recentmatch {
       id
     }
