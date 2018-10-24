2d8a7c88cc7be8df3b9427099d0038d8b29bf143
Stefan Walker <stefan.walker@gmail.com>
2018-10-24 15:40:51 +0100

    Signup fix

--- src/components/controllers/Login/NewSignupPageController.js
+++ src/components/controllers/Login/NewSignupPageController.js
@@ -6,10 +6,9 @@ import { toast } from 'react-toastify';
 import { GlobalStyles } from 'Theme/Theme';
 import historyStore from '../../../utils/stores/browserHistory';
 
-import { getUserQuery } from '../../../queries/users';
-// import { getUserQuery, updateUserQuery } from '../../../queries/users';
+import { getUserQuery, updateUserQuery } from '../../../queries/users';
 import { authenticateQuery } from '../../../queries/login';
-// import { deleteEmailRegistrationQuery } from '../../../queries/registrations';
+import { deleteEmailRegistrationQuery } from '../../../queries/registrations';
 
 class NewSignupPageController extends Component {
     componentDidMount = async () => {
@@ -17,11 +16,13 @@ class NewSignupPageController extends Component {
         const d = JSON.parse(Buffer.from(token, 'hex').toString('utf8'));
         const authPayload = await this.props.appManager.executeQuery('mutation', authenticateQuery, { email: d.email, password: d.password });
         const my_token = authPayload.authenticate.resultData.jwtToken;
+        const ar = this.props.appManager.decodeJWT(my_token);
+
         this.props.appManager.authToken = my_token;
         const user = await this.props.appManager.executeQueryAuth('query', getUserQuery, { id: d.id });
         const u = user.resultData;
-        if (u.authenticated === true) {
-            toast.error(`${d.organisation} is already registered - Redirecting you to login page in 5 seconds`, {
+        if (u.authenticated === true && ar.organisation !== null) {
+            toast.error(`${ar.organisation} is already registered - Redirecting you to login page in 5 seconds`, {
                 position: toast.POSITION.TOP_LEFT,
                 autoClose: 5000
             });
@@ -29,12 +30,12 @@ class NewSignupPageController extends Component {
                 historyStore.push('/signup');
             }, 5000);
         } else {
-            // const payload = {
-            //     authenticated: true,
-            //     id: d.id,
-            // };
-            // await this.props.appManager.executeQuery('mutation', updateUserQuery, payload);
-            // await this.props.appManager.executeQuery('mutation', deleteEmailRegistrationQuery, { email: u.email });
+            const payload = {
+                authenticated: true,
+                id: d.id,
+            };
+            await this.props.appManager.executeQueryAuth('mutation', updateUserQuery, payload);
+            await this.props.appManager.executeQuery('mutation', deleteEmailRegistrationQuery, { email: u.email });
             const new_payload = Buffer.from(JSON.stringify(authPayload), 'utf8').toString('hex');
             historyStore.push(`/createsubdomain?p=${new_payload}`);
         }
