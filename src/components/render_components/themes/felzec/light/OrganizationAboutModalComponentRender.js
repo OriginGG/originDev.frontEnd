fe8a9cfcd54cb0d7c12106bc26dee37c4ab3aea6
System Administrator <root@kyles-MacBook-Pro.local>
2018-10-29 11:41:07 -0700

    changes to about modal for felzec theme

--- src/components/render_components/themes/felzec/light/OrganizationAboutModalComponentRender.js
+++ src/components/render_components/themes/felzec/light/OrganizationAboutModalComponentRender.js
@@ -10,24 +10,26 @@ class LightOrganizationAboutModalComponentRender extends Component {
             <div className={this.props.classes.blankBG}>
                 <div className={this.props.classes.felzec_about_header} style={this.props.bg_style}/>
                 <div className={this.props.classes.felzec_about_container}>
-                    <div className={this.props.classes.felzec_about_container_inner}>
-                        <div className={this.props.classes.felzec_about_title}>ABOUT US</div>
-                        <div className={this.props.classes.felzec_about_text}>{this.props.about_content}</div>
-                    </div>
-                    <div className={this.props.classes.felzec_about_container_lower}>
-                        <div className="ui stackable two column grid">
-                            <div className="column">
-                                <div className={this.props.classes.felzec_lower_about_title}>CONTACT US</div>
-                                <div className={this.props.classes.felzec_lower_about_text}>If you want to contact us you can always send us an email</div>
-                            </div>
-                            <div className="column">
-                                <div className={this.props.classes.felzec_lower_email_button}>
-                                    <div className={this.props.classes.felzec_about_email_title}>SUPPORT</div>
-                                    <div className={this.props.classes.felzec_about_email_text} onClick={this.props.handleSupportClick}>{this.props.about_support_email}</div>
+                    <div className={this.props.classes.felzec_about_desc_conatiner} style={this.props.about_desc_style}>
+                        <div className={this.props.classes.felzec_about_container_inner}>
+                            <div className={this.props.classes.felzec_about_title}>ABOUT US</div>
+                            <div className={this.props.classes.felzec_about_text}>{this.props.about_content}</div>
+                        </div>
+                        <div className={this.props.classes.felzec_about_container_lower}>
+                            <div className="ui stackable two column grid">
+                                <div className="column">
+                                    <div className={this.props.classes.felzec_lower_about_title}>CONTACT US</div>
+                                    <div className={this.props.classes.felzec_lower_about_text}>If you want to contact us you can always send us an email</div>
                                 </div>
-                                <div className={this.props.classes.felzec_lower_email_button}>
-                                    <div className={this.props.classes.felzec_about_email_title}>MANAGEMENT</div>
-                                    <div className={this.props.classes.felzec_about_email_text} onClick={this.props.handleBusinessClick}>{this.props.about_business_email}</div>
+                                <div className="column">
+                                    <div className={this.props.classes.felzec_lower_email_button}>
+                                        <div className={this.props.classes.felzec_about_email_title}>SUPPORT</div>
+                                        <div className={this.props.classes.felzec_about_email_text} onClick={this.props.handleSupportClick}>{this.props.about_support_email}</div>
+                                    </div>
+                                    <div className={this.props.classes.felzec_lower_email_button}>
+                                        <div className={this.props.classes.felzec_about_email_title}>MANAGEMENT</div>
+                                        <div className={this.props.classes.felzec_about_email_text} onClick={this.props.handleBusinessClick}>{this.props.about_business_email}</div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
